6d04db37e26d49cf6b1fbc87884b0144e804f275
Gabriel Orenic <gaorenic@gmail.com>
2021-02-18 10:56:19 -0600

    Got API Key

--- Programming Jokes/ContentView.swift
+++ Programming Jokes/ContentView.swift
@@ -26,12 +26,7 @@ struct ContentView: View {
         })
     }
     func getJokes() {
-        jokes.append(Joke(setup: "Why do programmers always get Christmas and Halloween mixed up?",
-                          punchline: "Because DEC 25 = OCT 31"))
-        jokes.append(Joke(setup: "How did the programmer die in the shower?",
-                          punchline: "She followed the shampoo bottle instructions: Lather. Rinse. Repeat."))
-        jokes.append(Joke(setup: "There are 10 types of people in the world",
-                          punchline: "Those who understand binary and those who don’t."))
+        let apiKey = "?rapidapi-key=a0809decc4msh6a913972fdda630p1e1af4jsn1f65e79d66ab"
     }
 }
 
